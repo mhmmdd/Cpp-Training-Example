c3796c3118cdb9f8ab1430b509a86e11a580b8c8
Muhammed Doğan <mhmmd.dd@gmail.com>
2018-03-04 17:45:05 +0300

    Updated Enum

--- 02_Enum/02_Enum/Main.cpp
+++ 02_Enum/02_Enum/Main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 enum Example: char {
 	A = 5,
@@ -6,8 +9,55 @@ enum Example: char {
 	C
 };
 
+enum dayOfWeek {
+	M=1, TU, W, TH, F, SA, SN
+};
+
+string getDay(dayOfWeek d) {
+	switch (d) {
+		case M:
+			return "Monday";
+		case TU:
+			return "Tuesday";
+		case W:
+			return "Wednesday";
+		case TH:
+			return "Thursday";
+		case F:
+			return "Friday";
+		case SA:
+			return "Saturday";
+		case SN:
+			return "Sunday";
+		default:
+			return "You typed something wrong!";
+	}
+}
+
 int main() {
 
-	std::cin.get();
-	return;
+	dayOfWeek d = TH;
+
+	cout << d << endl;
+
+	cout << dayOfWeek(F) << endl;
+	cout << dayOfWeek(SA) << endl;
+	cout << dayOfWeek(SN) << endl;
+	cout << endl;
+
+
+	int i;
+	cout << "Enter the day of a week " << endl;
+	cout << "1. Monday " << endl;
+	cout << "2. Tuesday" << endl;
+	cout << "3. Wednesday " << endl;
+	cout << "4. Thursday " << endl;
+	cout << "5. Friday " << endl;
+	cout << "6. Saturday " << endl;
+	cout << "7. Sunday " << endl;
+	cin >> i;
+	cout << getDay(dayOfWeek(i)) << endl;
+
+	system("pause");
+	return 0;
 }
